ec9c6ce069c80c175efe6503197a7340b06701f3
TOM <tuomis29@gmail.com>
2017-05-02 21:37:35 +0300

    added request to api

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,31 @@
 const router = require("express").Router();
+const request = require("request");
 
-router.get('*', function (req, res) {
+const options = {  
+    url: "https://api.yelp.com/v3/businesses/search?location=la&categories=nightlife",
+    method: "GET",
+    headers: {
+        "Accept": "application/json",
+        "Accept-Charset": "utf-8",
+        "Authorization": require("../config.json").access_token
+    }
+};
+
+router.get("*", function (req, res, next) {
+    if(req.headers.data_fetch === "true"){
+        return next();
+    }
     res.render('index.ejs');
 });
+
+router.get("/",function(req,res){
+    let json;
+    request(options, function(err, res2, body) {  
+        json = JSON.parse(body);
+        res.status(200).json(json);
+    });
+    
+});
+
+
 module.exports = router;
