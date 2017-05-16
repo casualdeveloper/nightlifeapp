908aaaf8c43e4aca9ba7045cf52eb72ba5789541
TOM <tuomis29@gmail.com>
2017-05-16 22:26:52 +0300

    webpack update (seperated bundle from app)

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,71 @@
 const path = require("path");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const webpack = require("webpack");
 
+const PATHS = {
+    app:path.join(__dirname,"src/client.js"),
+    style:path.join(__dirname,"src/styles/style.css")
+}
+
+const autoprefix = function() {
+    return {
+        loader: "postcss-loader",
+        options: {
+            plugins: () => ([
+                require("autoprefixer"),
+            ]),
+        },
+    };
+};
 
 module.exports = {
-    entry: "./src/client.js",
+    entry: {
+        app:PATHS.app,
+        style:PATHS.style
+    },
+    devtool:"cheap-source-map",
     output: {
-        filename: "bundle.js",
+        filename: "[name].js",
         path: path.join(__dirname, "/public")
     },
     module: {
-        rules: [{
-            test: /\.js$||.jsx$/,
-            exclude: /node_modules/,
-            loader: "babel-loader",
-            options: {
-                presets: ["react","es2015"]
+        rules: [
+            {
+                test: /\.css$/,
+                include: PATHS.style,
+                use: ExtractTextPlugin.extract({
+                    use: ["css-loader", autoprefix()],
+                    fallback: "style-loader",
+                }),
+            },
+            {
+                test: /\.scss$/,
+                include: PATHS.style,
+                use: ExtractTextPlugin.extract({
+                    use: ["css-loader", autoprefix(), "sass-loader"],
+                    fallback: "style-loader",
+                }),
+            },
+            {
+                test: /(\.js|.jsx)$/,
+                loader: "babel-loader",
+                options: {
+                    presets: ["react","es2015"]
             }
-
         }]
-    }
+    },
+    plugins:[
+        new ExtractTextPlugin({
+            filename: (process.env.NODE_ENV === "production") ? "css/[name].[contenthash].css" : "[name].css",
+        }),
+        new webpack.optimize.CommonsChunkPlugin({
+            name: "bundle",
+
+            minChunks: ({ resource }) => (
+                resource &&
+                resource.indexOf("node_modules") >= 0 &&
+                resource.match(/\.js$/)
+            ),
+        })
+    ]
 };
